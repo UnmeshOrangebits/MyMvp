777c331aaba8d711c241ddd7cf8ecc118d985b05
UnmeshOrangebits <unmesh.mishra@orangebitsindia.com>
2020-06-18 12:59:56 +0530

    first change

--- app/src/main/java/com/task/mymvp/MainActivity.java
+++ app/src/main/java/com/task/mymvp/MainActivity.java
@@ -29,6 +29,7 @@ public class MainActivity extends AppCompatActivity implements MainView{
                 presenter.onButtonClick();
             }
         });
+        //changes to the mainactivity
 
     }
 
